a3f21047b36c8ac343c37fb0c389b7d21889944d
irfannudin ihsan <irfannudinihsan56@gmail.com>
2023-02-17 13:51:27 +0700

    add custom hook useScrollAnchor and useModal

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 import Header from "parts/Header";
-import Hero from "parts/Hero";
+import Hero from "parts/HomePage/Hero";
 import BrowseRoom from "parts/HomePage/BrowseRoom";
 import JustArrived from "parts/HomePage/JustArrived";
 import Clients from "parts/HomePage/Clients";
 import SiteMap from "parts/HomePage/SiteMap";
 import Footer from "parts/HomePage/Footer";
 
+import useModalDOM from "helpers/hooks/useModalDOM";
+
+
+import useScrollAnchor from "helpers/hooks/useScrollAnchor";
+
 export default function HomePage() {
+  useScrollAnchor()
+  useModalDOM()
   return (
     <>
       <Header theme="white" position="absolute"/>
